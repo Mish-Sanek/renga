bb0632220c2d4bc4da3b7d0c6b2be77caa8afb6d
renuaGIT <117173821+renuaGIT@users.noreply.github.com>
2026-04-28 14:35:27 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,29 @@ const nextConfig = {
     return [
       {
         source: '/blog/:path*',
-        destination: 'https://renua-one.ghost.io/:path*',  // Без /blog в destination!
+        destination: 'https://renua-one.ghost.io/:path*', // <- КЛЮЧЕВОЙ МОМЕНТ: без /blog
+      },
+    ];
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/blog/:path*',
+        headers: [
+          {
+            key: 'X-Forwarded-Host',
+            value: 'renua.one',
+          },
+          {
+            key: 'X-Forwarded-Proto',
+            value: 'https',
+          },
+          {
+            key: 'X-Forwarded-For',
+            value: '', // Vercel подставит IP автоматически
+          },
+        ],
       },
     ];
   },
