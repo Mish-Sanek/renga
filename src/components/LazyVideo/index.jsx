a210d6cff000b00fa5fdc725a6aea536f8cf1fc7
Mish_Sanek <kid02@list.ru>
2026-05-22 23:21:38 +0500

    fixing shakings v6

--- src/components/LazyVideo/index.jsx
+++ src/components/LazyVideo/index.jsx
@@ -1,47 +1,14 @@
 'use client';
-import { useEffect, useRef } from 'react';
 
 const LazyVideo = ({ src, poster, threshold = 0.05, aspectRatio = "16/9", ...props }) => {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    const video = videoRef.current;
-    if (!video) return;
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-
-            video.play().catch((e) => {
-              console.log('Autoplay blocked:', e);
-            });
-          } else {
-            video.pause();
-          }
-        });
-      },
-      {
-        threshold,
-        rootMargin: '100px'
-      }
-    );
-
-    observer.observe(video);
-
-    return () => {
-      observer.disconnect();
-    };
-  }, [threshold]);
-
   return (
     <div >
       <video
-        ref={videoRef}
         src={src}
         poster={poster}
         muted
         loop
+        autoPlay
         playsInline
         preload="none"
         {...props}
